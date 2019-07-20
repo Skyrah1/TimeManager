393f2daa1b8bd9148a5477bcb2bd26891fbe77e7
Skyrah1 <zhunchenyek1@gmail.com>
2019-07-20 21:27:01 +0800

    Update FileHandler.java

--- src/timemanager/FileHandler.java
+++ src/timemanager/FileHandler.java
@@ -13,6 +13,8 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Hashtable;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 
@@ -29,13 +31,16 @@ public class FileHandler {
 	this.path = path;
     }
     
-    public String[][] readFile(String file){
-	String[][] schedule = new String[0][0];	    //return this ONLY if file
-						    //is not found
+    public Hashtable<Date, String> readFile(String file){
+	Hashtable<Date,String> schedule = new Hashtable<>();
 	try {
 	    FileInputStream inputStream = new FileInputStream(file);
 	    InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
 	    CSVParser parser = new CSVParser(reader, CSVFormat.EXCEL);
+	    for (CSVRecord record : parser){
+		//enter data from record into hashtable,
+		//converting the first field into a Date object
+	    }
 	} catch (Exception e){
 	    
 	}
