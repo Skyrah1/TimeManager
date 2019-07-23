103e60f66dfcbc740c4d4b2790165d6bf0536954
Skyrah1 <zhunchenyek1@gmail.com>
2019-07-23 19:40:11 +0800

    Started work on PopUpWindow

--- src/timemanager/ui/PopUpWindow.java
+++ src/timemanager/ui/PopUpWindow.java
@@ -5,10 +5,34 @@
  */
 package timemanager.ui;
 
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+
 /**
  * A pop-up window for when the user wants to edit their schedule
  * @author Yek
  */
 public class PopUpWindow {
+    
+    final JFrame frame;
+    final JComboBox hourComboBox;
+    final JComboBox minuteComboBox;
+    final JComboBox amPmComboBox;
+    final String[] hours = "1,2,3,4,5,6,7,8,9,10,11,12".split(",");
+    final String[] minutes;
+    final String[] amPm = {"AM", "PM"};
+    
+    public PopUpWindow(String title){
+	this.minutes = new String[60];
+	for (int i = 1; i <= 60; i++){
+	    minutes[i] = Integer.toString(i);
+	}
+	this.frame = new JFrame(title);
+	this.hourComboBox = new JComboBox();
+	this.minuteComboBox = new JComboBox();
+	this.amPmComboBox = new JComboBox();
+    }
+    
+    
     
 }
