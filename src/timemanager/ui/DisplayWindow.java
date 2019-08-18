650857b1204f9b095118f5d94174865a75dfeb60
Skyrah1 <zhunchenyek1@gmail.com>
2019-08-18 08:25:34 +0100

    First steps towards adding picture to DisplayWindow

--- src/timemanager/ui/DisplayWindow.java
+++ src/timemanager/ui/DisplayWindow.java
@@ -7,13 +7,19 @@ package timemanager.ui;
 
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.TreeMap;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 /**
@@ -27,8 +33,11 @@ public class DisplayWindow {
     final Font font;
     final PopUpWindow window;
     final JButton button;
+    ArrayList<BufferedImage> images;
+    private int imageIndex;
     
-    public DisplayWindow(String title, TreeMap<Date, String> schedule){
+    public DisplayWindow(String title, TreeMap<Date, String> schedule,
+			ArrayList<BufferedImage> images){
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	int textX = 25;
 	int textY = 25;
@@ -41,6 +50,8 @@ public class DisplayWindow {
 	this.font = new Font("Arial", Font.PLAIN, 18);
 	this.window = new PopUpWindow("test2", new String[]{"Time", "Activity"}, "Submit");
 	this.button = new JButton("Pop-up");
+	this.images = images;
+	this.imageIndex = 0;
 	window.createWindow(schedule);
 	window.hideWindow();
 	textArea.setFont(font);
@@ -73,6 +84,13 @@ public class DisplayWindow {
 	    textArea.setVisible(false);
 	}
 	frame.add(textArea);
+	updateImage();
+    }
+    
+    public void updateImage(){
+	JLabel currentImage = new JLabel(new ImageIcon(images.get(imageIndex)));
+	frame.add(currentImage);
+	imageIndex = (imageIndex + 1) % images.size();
     }
     
     public void showWindow(){
